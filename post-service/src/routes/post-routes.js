4873cdee2bd54a63b139ad5f24e5992325ad23b5
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-02-23 01:33:10 -0800

    post service routes update

---
+++ post-service/src/routes/post-routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { createPost, getAllPosts } = require("../controllers/post-controller");
+const { authenticateRequest } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+// middleware -> this will tell if the user is an auth user or not
+router.use(authenticateRequest);
+
+router.post("/create-post", createPost);
+router.get("/posts", getAllPosts);
+
+module.exports = router;
