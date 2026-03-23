8ae26a53b4f9a3ec12b1c82258cefabb24df30e7
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-03-23 00:24:55 -0700

    auth middleware

---
+++ api-gateway/src/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+const logger = require("../utils/logger");
+const jwt = require("jsonwebtoken");
+
+const validateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    logger.warn("Access attempt without valid token!");
+    return res.status(401).json({
+      success: false,
+      message: "Authentication required!",
+    });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      logger.warn("Invalid token!");
+      return res.status(429).json({
+        success: false,
+        message: "Invalid token!",
+      });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = { validateToken };
