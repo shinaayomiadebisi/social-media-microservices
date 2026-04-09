78d6572250b4b59f2dd0fc199e9b06491d416ae1
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-04-09 04:11:05 -0700

    media controller update

--- media-service/src/controllers/media-controller.js
+++ media-service/src/controllers/media-controller.js
@@ -1,1 +1,18 @@
-const uploadMedia = "";
+const logger = require("../utils/logger")
+
+const uploadMedia = async(req, res) => {
+  logger.info("Starting media uploading")
+  try {
+    if(!req.file) {
+      logger.error("No file found. Please add a file and try again!")
+      return res.status(400).json({
+        success: false,
+        message: "No file found. Please add a file and try again!"
+      })
+    }
+
+    const {originalName, mimeType, buffer} = req.file
+  } catch(e) {
+
+  }
+};
