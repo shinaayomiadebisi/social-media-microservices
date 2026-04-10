1cba7f45f59069f3a6da7a65322cd9e7703caf6b
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-04-10 03:48:17 -0700

    update

--- media-service/src/controllers/media-controller.js
+++ media-service/src/controllers/media-controller.js
@@ -1,3 +1,5 @@
+const Media = require("../models/Media");
+const { uploadMediaToCloudinary } = require("../utils/cloudinary");
 const logger = require("../utils/logger")
 
 const uploadMedia = async(req, res) => {
@@ -13,6 +15,30 @@ const uploadMedia = async(req, res) => {
 
     const {originalName, mimeType, buffer} = req.file
     const userId = req.user.userId
+
+    logger.info(`File details: name=${originalName}, type=${mimeType}`);
+    logger.info('Uploading to cloudinary starting...')
+
+    const cloudinaryUploadResult = await uploadMediaToCloudinary(req.file)
+    logger.info(`CLoudinary upload successfully. Public Id: - ${cloudinaryUploadResult.public_id}`)
+
+    const newlyCreatedMedia = new Media({
+      publicId: cloudinaryUploadResult.public_id,
+      originalName,
+      mimeType,
+      url: cloudinaryUploadResult.secure_url,
+      userId
+    })
+
+    await newlyCreatedMedia.save()
+
+    res.status(201).json({
+      success: true,
+      mediaId: newlyCreatedMedia._id,
+      url: newlyCreatedMedia.url,
+      message: 'Media upload is successfully'
+    })
+
   } catch(e) {
 
   }
