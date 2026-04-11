cf382e35c7f1507c7ec8a0df921432990f147870
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-04-11 01:17:48 -0700

    media router

---
+++ media-service/src/routes/media-routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const multer = require('multer')
+
+const {} = require('../controllers/media-controller')
+const {} = require('../middleware/authMiddleware')
+const logger = require('../utils/logger')
+
+const router = express.Router()
+
