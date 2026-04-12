6789d496e017a13734d9d1d68714eb921d5a1fd9
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-04-12 08:15:04 -0700

    changes

--- media-service/src/routes/media-routes.js
+++ media-service/src/routes/media-routes.js
@@ -1,9 +1,18 @@
-const express = require('express')
-const multer = require('multer')
+const express = require("express");
+const multer = require("multer");
 
-const {} = require('../controllers/media-controller')
-const {} = require('../middleware/authMiddleware')
-const logger = require('../utils/logger')
+const {} = require("../controllers/media-controller");
+const { authenticateRequest } = require("../middleware/authMiddleware");
+const logger = require("../utils/logger");
 
-const router = express.Router()
+const router = express.Router();
 
+// configure multer for file upload
+const upload = multer({
+  storage: multer.memoryStorage,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+}).single("file");
+
+router.post("/upload", authenticateRequest);
