9d8092c7896f298052a56eadb09e904d58d023c9
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-04-14 00:47:57 -0700

    changes

--- media-service/src/routes/media-routes.js
+++ media-service/src/routes/media-routes.js
@@ -19,6 +19,24 @@ router.post("/upload", authenticateRequest, (req, res, next) => {
   upload(req, res, function (err) {
     if (err instanceof multer.MulterError) {
       logger.error("Multer error while uploading:", err);
+      return res.status(400).json({
+        message: "Multer error while uploading:",
+        error: err.message,
+        stack: err.stack,
+      });
+    } else if (err) {
+      logger.error("Unknown error occurred while uploading:", err);
+      return res.status(500).json({
+        message: "Unknown error occurred while uploading:",
+        error: err.message,
+        stack: err.stack,
+      });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({
+        message: "No file found!",
+      });
     }
   });
 });
