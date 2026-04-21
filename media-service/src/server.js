627c088fadfc44dfb1046b57d6f51a1ce8203470
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-04-21 11:12:24 -0700

    update

--- media-service/src/server.js
+++ media-service/src/server.js
@@ -7,9 +7,11 @@ const mediaRoutes = require('./routes/media-routes')
 const errorHandler = require("./middleware/errorHandler");
 const logger = require("./utils/logger");
 const express = require('express')
+const { rateLimit } = require("express-rate-limit");
+const { RedisStore } = require("rate-limit-redis");
+const Redis = require("ioredis");
 
 const app = express()
-
 const PORT = process.env.PORT || 3003
 
 // connect to mongodb
@@ -18,6 +20,38 @@ mongoose
   .then(() => logger.info("Connected to mongodb"))
   .catch((e) => logger.error("Mongo connection error", e));
 
+const redisClient = new Redis(process.env.REDIS_URL);
+
 app.use(cors())
 app.use(helmet())
 app.use(express.json())
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body, ${req.body}`);
+  next();
+});
+
+// Ip based rate limiting for sensitive endpoints
+const sensitiveEndpointsLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({
+      success: false,
+      message: "Too many requests",
+    });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+// apply this sensitiveEndpointsLimiter to our routes
+app.use("/api/media/upload", sensitiveEndpointsLimiter);
+
+app.use("/api/media", mediaRoutes)
+
