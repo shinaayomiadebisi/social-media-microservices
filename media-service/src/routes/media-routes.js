5856818ce7b345616964326e208c738f076122e9
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-04-23 05:42:00 -0700

    media routes update

--- media-service/src/routes/media-routes.js
+++ media-service/src/routes/media-routes.js
@@ -9,38 +9,43 @@ const router = express.Router();
 
 // configure multer for file upload
 const upload = multer({
-  storage: multer.memoryStorage,
+  storage: multer.memoryStorage(),
   limits: {
     fileSize: 5 * 1024 * 1024,
   },
 }).single("file");
 
-router.post("/upload", authenticateRequest, (req, res, next) => {
-  upload(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-      logger.error("Multer error while uploading:", err);
-      return res.status(400).json({
-        message: "Multer error while uploading:",
-        error: err.message,
-        stack: err.stack,
-      });
-    } else if (err) {
-      logger.error("Unknown error occurred while uploading:", err);
-      return res.status(500).json({
-        message: "Unknown error occurred while uploading:",
-        error: err.message,
-        stack: err.stack,
-      });
-    }
+router.post(
+  "/upload",
+  authenticateRequest,
+  (req, res, next) => {
+    upload(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        logger.error("Multer error while uploading:", err);
+        return res.status(400).json({
+          message: "Multer error while uploading:",
+          error: err.message,
+          stack: err.stack,
+        });
+      } else if (err) {
+        logger.error("Unknown error occurred while uploading:", err);
+        return res.status(500).json({
+          message: "Unknown error occurred while uploading:",
+          error: err.message,
+          stack: err.stack,
+        });
+      }
 
-    if (!req.file) {
-      return res.status(400).json({
-        message: "No file found!",
-      });
-    }
+      if (!req.file) {
+        return res.status(400).json({
+          message: "No file found!",
+        });
+      }
 
-    next()
-  });
-}, uploadMedia);
+      next();
+    });
+  },
+  uploadMedia,
+);
 
-module.exports = router
+module.exports = router;
