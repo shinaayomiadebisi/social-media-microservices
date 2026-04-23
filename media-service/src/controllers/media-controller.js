4ecb6e6d16f3df1cf2a717b59062a332733170d4
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-04-23 05:42:49 -0700

    media controllers update

--- media-service/src/controllers/media-controller.js
+++ media-service/src/controllers/media-controller.js
@@ -1,51 +1,53 @@
 const Media = require("../models/Media");
 const { uploadMediaToCloudinary } = require("../utils/cloudinary");
-const logger = require("../utils/logger")
+const logger = require("../utils/logger");
 
-const uploadMedia = async(req, res) => {
-  logger.info("Starting media uploading")
+const uploadMedia = async (req, res) => {
+  logger.info("Starting media uploading");
   try {
-    if(!req.file) {
-      logger.error("No file found. Please add a file and try again!")
+    console.log(req.file, "req.filereq.file");
+    if (!req.file) {
+      logger.error("No file found. Please add a file and try again!");
       return res.status(400).json({
         success: false,
-        message: "No file found. Please add a file and try again!"
-      })
+        message: "No file found. Please add a file and try again!",
+      });
     }
 
-    const {originalName, mimeType, buffer} = req.file
-    const userId = req.user.userId
+    const { originalName, mimeType, buffer } = req.file;
+    const userId = req.user.userId;
 
     logger.info(`File details: name=${originalName}, type=${mimeType}`);
-    logger.info('Uploading to cloudinary starting...')
+    logger.info("Uploading to cloudinary starting...");
 
-    const cloudinaryUploadResult = await uploadMediaToCloudinary(req.file)
-    logger.info(`CLoudinary upload successfully. Public Id: - ${cloudinaryUploadResult.public_id}`)
+    const cloudinaryUploadResult = await uploadMediaToCloudinary(req.file);
+    logger.info(
+      `CLoudinary upload successfully. Public Id: - ${cloudinaryUploadResult.public_id}`,
+    );
 
     const newlyCreatedMedia = new Media({
       publicId: cloudinaryUploadResult.public_id,
       originalName,
       mimeType,
       url: cloudinaryUploadResult.secure_url,
-      userId
-    })
+      userId,
+    });
 
-    await newlyCreatedMedia.save()
+    await newlyCreatedMedia.save();
 
     res.status(201).json({
       success: true,
       mediaId: newlyCreatedMedia._id,
       url: newlyCreatedMedia.url,
-      message: 'Media upload is successfully'
-    })
-
-  } catch(error) {
-      logger.error("Error deleting post", error);
-      res.status(500).json({
-        success: false,
-        message: "Error uploading media",
-      });
+      message: "Media upload is successfully",
+    });
+  } catch (error) {
+    logger.error("Error deleting media", error);
+    res.status(500).json({
+      success: false,
+      message: "Error uploading media",
+    });
   }
 };
 
-module.exports = { uploadMedia }
+module.exports = { uploadMedia };
