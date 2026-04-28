e8117e0f956b7134bd0886940ad5c76072a9ef58
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-04-28 04:21:55 -0700

    rabbitmq js update

---
+++ post-service/src/utils/rabbitmq.js
@@ -0,0 +1,9 @@
+const amqp = require("amqplib");
+const logger = require("./logger");
+
+let connection = null;
+let channel = null;
+
+const EXCHANGE_NAME = "facebook_events";
+
+async function connectRabbitMQ() {}
