9f638533ea2f6406fbbc9816a16b608a17d7650c
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-05-29 02:16:28 -0700

    search event handler

---
+++ search-service/src/eventHandlers/search-event-handler.js
@@ -0,0 +1,43 @@
+const Search = require("../models/Search");
+const logger = require("../utils/logger");
+
+async function invalidateSearchCache(req, input) {
+  const cachedKey = `search:${input}`;
+  await req.redisClient.del(cachedKey);
+
+  // const keys = await req.redisClient.keys("searches:*");
+  // if (keys.length > 0) {
+  //   req.redisClient.del(keys);
+  // }
+}
+
+async function handlePostCreated(event) {
+  try {
+    const newSearchPost = new Search({
+      postId: event.postId,
+      userId: event.userId,
+      content: event.content,
+      createdAt: event.createdAt,
+    });
+
+    await newSearchPost.save();
+
+    await invalidateSearchCache(event, newSearchPost._id.toString());
+    logger.info(
+      `Search post created: ${event.postId}, ${newSearchPost._id.toString()}`,
+    );
+  } catch (e) {
+    logger.error(e, "Error handling post creation event");
+  }
+}
+
+async function handlePostDeleted(event) {
+  try {
+    await Search.findOneAndDelete({ postId: event.postId });
+    logger.info(`Search post deleted: ${event.postId}`);
+  } catch (error) {
+    logger.error(error, "Error handling post deletion event");
+  }
+}
+
+module.exports = { handlePostCreated, handlePostDeleted };
